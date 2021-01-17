e896deadaed89e3dca031e2b8a816b17aff303a0
Paweł Data <123pawel99@gmail.com>
2021-01-17 20:49:39 +0100

    Refactor operations file

--- src/Operations.cpp
+++ src/Operations.cpp
@@ -3,10 +3,8 @@
 std::string* CodeGenerator::Operations::add(Variable* var1, Variable* var2) {
     std::string* reg1 = this->codeGen.setVarToRegister(var1);
     std::string* reg2 = this->codeGen.setVarToRegister(var2);
-    std::string param = *reg1 + " " + *reg2;
 
-    this->codeGen.commands.push_back(new Command(ADD, param));
-
+    this->codeGen.commands.push_back(new Command(ADD, *reg1 + " " + *reg2));
     this->codeGen.memory->freeRegister(*reg2, -1);
     return reg1;
 }
@@ -14,10 +12,8 @@ std::string* CodeGenerator::Operations::add(Variable* var1, Variable* var2) {
 std::string* CodeGenerator::Operations::sub(Variable* var1, Variable* var2) {
     std::string* reg1 = this->codeGen.setVarToRegister(var1);
     std::string* reg2 = this->codeGen.setVarToRegister(var2);
-    std::string param = *reg1 + " " + *reg2;
 
-    this->codeGen.commands.push_back(new Command(SUB, param));
-
+    this->codeGen.commands.push_back(new Command(SUB, *reg1 + " " + *reg2));
     this->codeGen.memory->freeRegister(*reg2, -1);
     return reg1;
 }
@@ -61,13 +57,10 @@ std::string* CodeGenerator::Operations::div(Variable* var1, Variable* var2) {
     std::string reg1 = this->codeGen.getRegisterWithValue(0);
     std::string* reg2 = this->codeGen.setVarToRegister(var1);
 
-    std::string jzeroParam = *reg2 + " 8";
-    std::string joddParam = *reg2 + " 2";
-
     // first loop, inverse divisor
-    this->codeGen.commands.push_back(new Command(JZERO, jzeroParam));
+    this->codeGen.commands.push_back(new Command(JZERO, *reg2 + " 8"));
     this->codeGen.commands.push_back(new Command(SHL, reg1));
-    this->codeGen.commands.push_back(new Command(JODD, joddParam));
+    this->codeGen.commands.push_back(new Command(JODD, *reg2 + " 2"));
     this->codeGen.commands.push_back(new Command(JUMP, "2"));
     this->codeGen.commands.push_back(new Command(INC, reg1));
     this->codeGen.commands.push_back(new Command(SHR, *reg2));
@@ -82,38 +75,28 @@ std::string* CodeGenerator::Operations::div(Variable* var1, Variable* var2) {
     std::string tempSumReg = this->codeGen.getRegisterWithValue(0);
     std::string checkReg = this->codeGen.memory->getFreeRegister();
 
-    std::string checkZero = *reg2 + " 2";
-
     // if var2 == 0, return 0
-    this->codeGen.commands.push_back(new Command(JZERO, checkZero));
+    this->codeGen.commands.push_back(new Command(JZERO, *reg2 + " 2"));
     this->codeGen.commands.push_back(new Command(JUMP, "2"));
     this->codeGen.commands.push_back(new Command(JUMP, "17"));
 
-    std::string jzeroParam1 = counterReg + " 16";
-    joddParam = reg1 + " 2";
-
     // second loop, divide
-    this->codeGen.commands.push_back(new Command(JZERO, jzeroParam1));
+    this->codeGen.commands.push_back(new Command(JZERO, counterReg + " 16"));
     this->codeGen.commands.push_back(new Command(DEC, counterReg));
     this->codeGen.commands.push_back(new Command(SHL, tempSumReg));
     this->codeGen.commands.push_back(new Command(SHL, resultReg));
-    this->codeGen.commands.push_back(new Command(JODD, joddParam));
+    this->codeGen.commands.push_back(new Command(JODD, reg1 + " 2"));
     this->codeGen.commands.push_back(new Command(JUMP, "2"));
     this->codeGen.commands.push_back(new Command(INC, tempSumReg));
     this->codeGen.commands.push_back(new Command(SHR, reg1));
 
-    std::string addParam = checkReg + " " + *reg2;
-    std::string subParam1 = checkReg + " " + tempSumReg;
-    std::string jzeroParam2 = checkReg + " 2";
-    std::string subParam2 = tempSumReg + " " + *reg2;
-
     // check, if tempSum >= divident
     this->codeGen.commands.push_back(new Command(RESET, checkReg));
-    this->codeGen.commands.push_back(new Command(ADD, addParam));
-    this->codeGen.commands.push_back(new Command(SUB, subParam1));
-    this->codeGen.commands.push_back(new Command(JZERO, jzeroParam2));
+    this->codeGen.commands.push_back(new Command(ADD, checkReg + " " + *reg2));
+    this->codeGen.commands.push_back(new Command(SUB, checkReg + " " + tempSumReg));
+    this->codeGen.commands.push_back(new Command(JZERO, checkReg + " 2"));
     this->codeGen.commands.push_back(new Command(JUMP, "3"));
-    this->codeGen.commands.push_back(new Command(SUB, subParam2));
+    this->codeGen.commands.push_back(new Command(SUB, tempSumReg + " " + *reg2));
     this->codeGen.commands.push_back(new Command(INC, resultReg));
 
     this->codeGen.commands.push_back(new Command(JUMP, "-15"));
@@ -173,9 +156,7 @@ std::string* CodeGenerator::Operations::mod(Variable* var1, Variable* var2) {
     reg1 = this->codeGen.setVarToRegister(var1);
     reset->setParam(*reg1);
 
-    std::string subParam = *reg1 + " " + mulResultReg;
-
-    this->codeGen.commands.push_back(new Command(SUB, subParam));
+    this->codeGen.commands.push_back(new Command(SUB, *reg1 + " " + mulResultReg));
 
     uint jumpSize = this->codeGen.commands.size() - jumpPtr + 1;
     jump->setParam(std::to_string(jumpSize));
